28009fe2ed2561bf319ba11ae99395df9773bfae
Sulivan Leite <sulivan.lineage2@gmail.com>
2025-01-11 11:17:28 -0300

    fix: ajustes nas rotas do dashboard

--- frontend/src/app/features/dashboard/dashboard-routing.module.ts
+++ frontend/src/app/features/dashboard/dashboard-routing.module.ts
@@ -1,22 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardLayoutComponent } from '../../shared/layouts/dashboard-layout/dashboard-layout.component';
+import { DashboardLayoutComponent } from '../../layouts/dashboard-layout/dashboard-layout.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: DashboardLayoutComponent,
-    children: [
-      {
-        path: '',
-        component: DashboardComponent
-      },
-      {
-        path: 'dividas',
-        loadChildren: () => import('../dividas/dividas.module').then(m => m.DividasModule)
-      }
-    ]
+    component: DashboardComponent
   }
 ];
 
